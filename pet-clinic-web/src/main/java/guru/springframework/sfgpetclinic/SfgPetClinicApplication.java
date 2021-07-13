2fe8e16ec315a08a046d0502d23f2c7749b956f0
Adam Jennings <ajennings@tier2consulting.com>
2021-07-13 11:48:26 +0100

    Added Project Lombok

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/SfgPetClinicApplication.java
@@ -2,11 +2,20 @@ package guru.springframework.sfgpetclinic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 
 @SpringBootApplication
 public class SfgPetClinicApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SfgPetClinicApplication.class, args);
+
+
+    }
+    @Bean(name="entityManagerFactory")
+    public LocalSessionFactoryBean sessionFactory(){
+        LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
+        return sessionFactory;
     }
 }
